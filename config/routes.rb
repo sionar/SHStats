794cfe15491c4315f4d4cfcae977559a62e87bf7
tony-noxx <tony@getnoxx.com>
2022-05-21 17:51:02 -0400

    Update route for api users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index]
   end
 
   root "static_pages#root"
