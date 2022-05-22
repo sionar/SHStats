f20fa21a92081cc4e70536f42b18ed85f2d7c7a6
tony-noxx <tony@getnoxx.com>
2022-05-21 22:20:18 -0400

    Add csrf skip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   def param_ints
     ints = []
     10.times do |i|
