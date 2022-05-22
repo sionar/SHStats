ecddba6c7b40a0eaf2173f5af7fdc6d80c34b751
tony-noxx <tony@getnoxx.com>
2022-05-22 01:57:58 -0400

    Convert game_params into class variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,14 @@ class User < ApplicationRecord
   has_many :games, through: :players
 
   def total_wr(game_params)
-    players = self.players.where(game_params)
+    players = self.players.where(@@game_params)
     return 0 if players.count.zero?
 
     players.where(win: true).count / players.count.to_f
   end
 
   def self.sorted_by_total_wr(game_params)
-    User.all.sort_by(&:total_wr(game_params))
+    @@game_params = game_params
+    User.all.sort_by(&:total_wr)
   end
 end
