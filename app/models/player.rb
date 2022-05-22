0b48aea6c59c1aab941d7c4bc994336619775387
tony-noxx <tony@getnoxx.com>
2022-05-21 22:32:26 -0400

    Fix validation for player win

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Player < ApplicationRecord
-  validates :role, :user, :game, :win, presence: true
+  validates :role, :user, :game, presence: true
+  validates :win, inclusion: { in: [ true, false ] }
   validates :role, inclusion: { in: %w[liberal fascist hitler] }
 
   belongs_to :user
