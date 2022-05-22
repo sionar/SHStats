ddf574c66067b358eea0531a6d003618291cb84b
Tony Ye <eterna@gmail.com>
2022-05-22 15:14:13 -0400

    Add lookup functions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -24,7 +24,7 @@ module Api
       @users = []
       ints = param_ints
       ints.each do |i|
-        user = create_or_update_user(i)
+        user = params[:lookup] == 'true' ? User.find_by(steam_id: params[:steam_id0]) : create_or_update_user(i)
         @users.push(get_stats(user))
       end
       render :show, status: 200
